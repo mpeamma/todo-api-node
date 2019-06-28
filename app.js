d32a26f1ccf93f41358acdae404e746156734ca5
Michael Eamma <michael.eamma@credera.com>
2019-06-27 22:38:52 -0500

    Added integration with postgres DB

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import db from './db/db';
+import { Client } from 'pg';
 import cors from 'cors';
+
 // Set up the express app
 const app = express();
 
@@ -10,10 +12,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 
 app.get('/', (req, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'Heartbeat',
-  })
+  try {
+    const client = new Client({
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
+        user: process.env.DB_USER || 'postgres',
+        database: process.env.DB ||'postgres',
+        password: process.env.DB_USER || 'mysecretpassword',
+      });
+    client.query('SELECT NOW()')
+      .then(result => res.status(200).send({success: 'true', message: result.rows[0].now }))
+      .catch(e => res.status(200).send({success: 'false', message: 'DB Not Connected'}))
+      .then(() => client.end())
+    client.connect();
+  } catch(e) {
+    console.log(e);
+    res.status(200).send({success: 'false', message: 'DB Not Connected'});
+  }
 });
 
 // get all todos
@@ -37,17 +52,17 @@ app.post('/api/todos', (req, res) => {
       message: 'description is required'
     });
   }
- const todo = {
-   id: Math.max(...db.map(i => i.id)) + 1,
-   title: req.body.title,
-   description: req.body.description
- }
- db.push(todo);
- return res.status(201).send({
-   success: 'true',
-   message: 'todo added successfully',
-   todo
- })
+  const todo = {
+    id: Math.max(...db.map(i => i.id)) + 1,
+    title: req.body.title,
+    description: req.body.description
+  }
+  db.push(todo);
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo added successfully',
+    todo
+  })
 });
 
 app.delete('/api/todos/:id', (req, res) => {
